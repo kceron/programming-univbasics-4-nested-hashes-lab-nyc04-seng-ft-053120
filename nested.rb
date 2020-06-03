d6f6ec96b55f1ff1ae25e78b23f0b90eb7679148
Karem Ceron <karemiceron@gmail.com>
2020-06-03 20:25:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
       }
     }
   programmer_hash[:yukihiro_matsumoto]
-  programmer_hash[:yukihiro_matsumoto] << {:known_for => "Ruby"]
+  programmer_hash[:yukihiro_matsumoto] << [:known_for => nil]
 end
 
 def changing_alan
