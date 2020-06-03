7fc6df003bf36d5e770a2021ba62a689c15f0381
Karem Ceron <karemiceron@gmail.com>
2020-06-03 20:18:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
       }
     }
   programmer_hash[:yukihiro_matsumoto]
-  
-  
+  programmer_hash[:yukihiro_matsumoto] << [:known_for => "Ruby"]
+  epic_tragedy[:additional_characters] << { name: "Apothecary" }
 end
 
 def changing_alan
