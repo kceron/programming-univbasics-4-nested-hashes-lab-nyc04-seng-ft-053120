bbd4d79942f4a59f56fcb6e401a11b1f9a142554
Karem Ceron <karemiceron@gmail.com>
2020-06-03 20:42:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,9 +100,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
